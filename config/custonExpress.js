f659db34a6d3fb2b540870a48d243b81f44fd527
thayron Feitosa <thayronf3@gmail.com>
2020-06-16 13:06:24 -0300

    Foi adicionado a biblioteca body-parser para que fosse possível usar o método POST recebendo um json do cliente

--- config/custonExpress.js
+++ config/custonExpress.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const consing= require('consign');
+const bodyParser = require('body-parser');
+
 
 module.exports =() =>{
 
     const app = express();
-    
+    app.use(bodyParser.urlencoded({extends: true}))
+    app.use(bodyParser.json())
+
     consing()
         .include('controller')
         .into(app)
--- controller/atendimento.js
+++ controller/atendimento.js
@@ -1,5 +1,10 @@
 
 module.exports = app =>{
     
-    app.get('/atendimentos', (req, res)=> res.send('você está na rota atendimentos'))
+    app.get('/atendimentos', (req, res)=> {res.send('você está na rota atendimentos')});
+
+    app.post('/atendimentos', (req, res) => 
+    {   console.log(req.body)
+        res.send('voce está na rota de atendimentos e está realizando um post')
+    });
 }
